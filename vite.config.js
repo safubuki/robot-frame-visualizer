9ccdaf802ddd85a714d247380b981c818b211485
safubuki <safubuki.dev@gmail.com>
2025-09-18 21:33:21 +0900

    feat!: Vite設定にbaseプロパティ追加

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // この base プロパティを追加します
+  base: "/robot-frame-visualizer/",
   plugins: [react()],
 })
